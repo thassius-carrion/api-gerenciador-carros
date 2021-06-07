12b4135c90c649bb4ae8c1e6187e9909b0085313
THASSIUS <thassius.carrion@hotmail.com>
2021-06-07 16:00:05 -0300

    criando o projeto

---
+++ src/main/java/com/zup/apigerenciadorcarros/ApiGerenciadorCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.zup.apigerenciadorcarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorCarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/apigerenciadorcarros/ApiGerenciadorCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.apigerenciadorcarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
